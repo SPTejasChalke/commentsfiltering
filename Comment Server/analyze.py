389f23f96f0d6ff29e8c1580b8d31ecc42e01955
TejasChalke <tejaschalke66@gmail.com>
2023-10-20 14:45:40 +0530

    added cors support to the server

--- Comment Server/analyze.py
+++ Comment Server/analyze.py
@@ -1,25 +1,49 @@
+# import nltk
+# from nltk.sentiment.vader import SentimentIntensityAnalyzer
+# nltk.download('vader_lexicon')
+
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS, cross_origin
+
+# app = Flask(__name__)
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
+
+# @app.route("/analyze")
+# @cross_origin
+# def foo():
+#     text = request.args.get('text')
+    
+#     if text:
+#         sid = SentimentIntensityAnalyzer()
+#         score = sid.polarity_scores(text)['compound']
+#         return jsonify({"score": score})
+#     else:
+#         return jsonify({"error": "Text parameter missing"})
+
+# if __name__== "__main__":
+#     app.run()
+
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 nltk.download('vader_lexicon')
 
-from flask import Flask, render_template
-
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/analyze/<text>")
-def foo(text):
+@app.route("/analyze")
+def analyze():
+    text = request.args.get('text')
     
-    sid = SentimentIntensityAnalyzer()
-    score = ((sid.polarity_scores(str(text))))['compound']
+    if text:
+        sid = SentimentIntensityAnalyzer()
+        score = sid.polarity_scores(text)['compound']
+        return jsonify({"score": score})
+    else:
+        return jsonify({"error": "Text parameter missing"})
 
-    # if(score > 0):
-    #     label = 'This sentence is positive'
-    # elif(score == 0):
-    #     label = 'This sentence is neutral'
-    # else:
-    #     label = 'This sentence is negative'
-    
-    return score
-
-if __name__== "__main__":
-    app.run()
+if __name__ == "__main__":
+    app.run(port=5000)
